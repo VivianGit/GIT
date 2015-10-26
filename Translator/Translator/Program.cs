0446360e1d2c0d3c811739d6866f0e7a3855d11d
晴依 <L#>
2015-10-26 22:40:16 +0800

    游戏翻译

---
+++ Translator/Translator/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using System.Drawing;
+using SharpDX;
+using System.Net;
+using System.Runtime.Serialization.Json;
+
+namespace Translator {
+	class Program {
+		
+		public static Menu Config;
+
+		static void Main(string[] args) {
+			Config = new Menu("翻译", "翻译测试", true);
+			Config.AddItem(new MenuItem("enable","启用").SetValue(true));
+			Config.AddItem(new MenuItem("API","翻译来源").SetValue(
+				new StringList(new[] {"百度","有道","谷歌"},0)));
+			Config.AddItem(new MenuItem("src", "来源语言").SetValue(
+				new StringList(new[] { "自动", "中文", "英文", "韩文" }, 0)));
+			Config.AddItem(new MenuItem("dec", "目标语言").SetValue(
+				new StringList(new[] { "自动", "中文", "英文", "韩文" }, 0)));
+			Config.AddItem(new MenuItem("DonotProcess", "不显示原话").SetValue(true));
+
+			Config.AddItem(new MenuItem("NameList","翻译以下人员的消息：")
+				.SetFontStyle(0, SharpDX.Color.BlanchedAlmond));
+			foreach (var hero in HeroManager.AllHeroes)
+			{
+				Config.AddItem(new MenuItem(hero.Name, Utill.Utf2Ansi(hero.Name)+"("+ hero.ChampionName+")").SetValue(true));
+			}
+			Config.AddToMainMenu();
+
+			Game.OnChat += Game_OnChat;
+			Game.OnInput += Game_OnInput;
+        }
+
+		private static void Game_OnInput(GameInputEventArgs args) {
+			if (!Config.Item(ObjectManager.Player.Name).GetValue<bool>()) return;
+			
+			string from, to, TranslatedString = "";
+			from = ((Language)Config.Item("src").GetValue<StringList>().SelectedIndex).ToString();
+			to = ((Language)Config.Item("dec").GetValue<StringList>().SelectedIndex).ToString();
+
+			string msg = Utill.Utf2Ansi(args.Input);
+
+			switch (Config.Item("API").GetValue<StringList>().SelectedIndex)
+			{
+				case 0:
+					TranslatedString = Utill.TranslateByBaidu(msg, from, to);
+					break;
+				default:
+					TranslatedString = Utill.TranslateByBaidu(msg, from, to);
+					break;
+			}
+
+			if (!string.IsNullOrEmpty(TranslatedString))
+			{
+				Game.Say(TranslatedString);
+				args.Process = false;
+			}
+			else
+			{
+				Game.PrintChat("出错了！没有得到翻译结果");
+			}
+		}
+
+		private static void Game_OnChat(GameChatEventArgs args) {
+			if (args.Sender.IsMe) return;
+			if (!Config.Item(args.Sender.Name).GetValue<bool>()) return;
+	
+			string from, to , TranslatedString = "";
+			from = ((Language)Config.Item("src").GetValue<StringList>().SelectedIndex).ToString();
+			to = ((Language)Config.Item("dec").GetValue<StringList>().SelectedIndex).ToString();
+
+			switch (Config.Item("API").GetValue<StringList>().SelectedIndex)
+			{
+				case 0:
+					TranslatedString = Utill.TranslateByBaidu(Utill.Utf2Ansi(args.Message), from,to);
+                    break;
+				default:
+					TranslatedString = Utill.TranslateByBaidu(Utill.Utf2Ansi(args.Message), from, to);
+					break;
+			}
+			if (!string.IsNullOrEmpty(TranslatedString))
+			{
+				Game.PrintChat("[{0}]{1}({2}):{3}",
+				new TimeSpan(0, 0, (int)Game.ClockTime),
+				args.Sender.Name,
+				args.Sender.ChampionName,
+				TranslatedString);
+				if (Config.Item("DonotProcess").GetValue<bool>())
+				{
+					args.Process = false;
+				}
+			}
+			else
+			{
+				Game.PrintChat("出错了！没有得到翻译结果");
+            }
+		}
+
+		
+	}
+}
---
+++ Translator/Translator/Utill.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translator {
+	public static class Utill {
+		public static string Ansi2Utf(string s) {
+			var b = Encoding.Convert(Encoding.Default, Encoding.Unicode, Encoding.Default.GetBytes(s));
+			b = Encoding.Convert(Encoding.Unicode, Encoding.UTF8, b);
+			return Encoding.Default.GetString(b);
+
+		}
+
+		public static string Utf2Ansi(string s) {
+			var b = Encoding.Convert(Encoding.UTF8, Encoding.Unicode, Encoding.Default.GetBytes(s));
+			b = Encoding.Convert(Encoding.Unicode, Encoding.Default, b);
+			return Encoding.Default.GetString(b);
+		}
+
+		public static string TranslateByBaidu(string message, string from, string to) {
+			
+			string url = string.Format("http://openapi.baidu.com/public/2.0/bmt/translate?client_id=ZFP9CoV9s6guCG5z1uuKOAdQ&q={0}&from={1}&to={2}", message, from, to);
+
+			Console.WriteLine(url);
+
+			var wr = WebRequest.Create(url);
+			wr.Timeout = 4000;
+			wr.Method = "GET";
+			var response = wr.GetResponse();
+			using (var stream = response.GetResponseStream())
+			{
+				if (stream != null)
+				{
+					var ser = new DataContractJsonSerializer(typeof(TransResult));
+					var transResult = (TransResult)ser.ReadObject(stream);
+					return transResult.trans_result.First().dst;
+				}
+			}
+			return "";
+		}
+
+	}
+
+	[DataContract]
+	class TransResult {
+		[DataMember]
+		public string from { get; set; }
+		[DataMember]
+		public string to { get; set; }
+		[DataMember]
+		public List<Trans> trans_result { get; set; }
+	}
+	[DataContract]
+	class Trans {
+		[DataMember]
+		public string src { get; set; }
+		[DataMember]
+		public string dst { get; set; }
+	}
+}
